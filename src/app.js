378b2305eb8f184be7286e970edc111e8d7336da
Thibault Six <t.six@guest.uliege.be>
2020-08-13 01:01:33 +0200

    récupération des 10premiers pokémons de la liste

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
-require('alpinejs');
+require("alpinejs");
+
+const Pokedex = require("pokeapi-js-wrapper");
+const P = new Pokedex.Pokedex();
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  let Pokedex = {
+    list: [],
+  };
+
+  Pokedex.list = getlist();
+  window.Pokedex = Pokedex;
+});
+
+function getlist() {
+  let result = [];
+  P.getPokemonsList().then(function(response) {
+    window.Pokedex.list = response.results.slice(0, 10);
+  });
+  return result;
+}
