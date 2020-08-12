3650d133ddaf166ed6a0d9c921d9a74ee18fcf87
Thibault Six <t.six@guest.uliege.be>
2020-08-13 00:09:42 +0200

    layout de base du pokedex

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,14 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
-mix.js('src/app.js', 'dist')
-    .postCss('src/app.css', 'dist', [
-        require('tailwindcss'),
-    ])
-   .setPublicPath('dist');
+mix
+  .js("src/app.js", "dist")
+  .postCss("src/app.css", "dist", [require("tailwindcss")])
+  .extract(["alpinejs"])
+  .setPublicPath("dist")
+  .browserSync({
+    open: false,
+    ui: false,
+    proxy: "alpine-pokedex.test",
+    port: 3000,
+    files: ["index.html", "src/**"],
+  });
